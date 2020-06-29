82d7fa02e42787110d3e271b91bca592c027ee58
DESKTOP-HKOR834\goost <goosterhof@hotmail.com>
2020-06-29 18:04:43 +0200

    No more temp ps1 files

--- cockroach/generatedatabaseusers_windows.go
+++ cockroach/generatedatabaseusers_windows.go
@@ -4,11 +4,8 @@ package cockroach
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
-	"path/filepath"
 
 	"github.com/juju/errors"
 )
@@ -16,13 +13,7 @@ import (
 func (c *Cockroach) generateDatabaseUsers() error {
 	log.Println("Generating database, users, roles and assigning privileges..")
 
-	tmpPSFile := filepath.FromSlash(os.TempDir() + "/tmp.ps1")
-	if err := ioutil.WriteFile(tmpPSFile,
-		[]byte(fmt.Sprintf(`cockroach sql --certs-dir=%s --host=%s:%d --execute="%s"`,
-			c.certsDir, c.host, c.portStart, setupDatabaseSQL)), 0600); err != nil {
-		return errors.Trace(err)
-	}
-	out, err := exec.Command("powershell", tmpPSFile).CombinedOutput()
+	out, err := exec.Command("cockroach", "sql", "--certs-dir="+c.certsDir, fmt.Sprintf("--host=%s:%d", c.host, c.portStart), `--execute=`+setupDatabaseSQL).CombinedOutput()
 	if err != nil {
 		log.Println(string(out))
 		return errors.Trace(err)
--- cockroach/generatehttpinterfaceuser_windows.go
+++ cockroach/generatehttpinterfaceuser_windows.go
@@ -10,11 +10,8 @@ package cockroach
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
-	"path/filepath"
 
 	"github.com/juju/errors"
 )
@@ -24,13 +21,7 @@ func (c *Cockroach) generateHTTPInterfaceUser() error {
 
 	SQLString := fmt.Sprintf(httpUserSQL, c.httpUser, c.httpPassword, c.httpUser)
 
-	tmpPSFile := filepath.FromSlash(os.TempDir() + "/tmp.ps1")
-	if err := ioutil.WriteFile(tmpPSFile,
-		[]byte(fmt.Sprintf(`cockroach sql --certs-dir=%s --host=%s:%d --execute="%s"`,
-			c.certsDir, c.host, c.portStart, SQLString)), 0600); err != nil {
-		return errors.Trace(err)
-	}
-	out, err := exec.Command("powershell", tmpPSFile).CombinedOutput()
+	out, err := exec.Command("cockroach", "sql", "--certs-dir="+c.certsDir, fmt.Sprintf("--host=%s:%d", c.host, c.portStart), `--execute=`+SQLString).CombinedOutput()
 	if err != nil {
 		log.Println(string(out))
 		return errors.Trace(err)
