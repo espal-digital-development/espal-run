37dd75939e1e18dc15f19b8b8276039411baa0e2
Erwin <espaldd@icloud.com>
2020-07-12 12:50:56 +0200

    Higher file limits

--- setsoftlimit_darwin.go
+++ setsoftlimit_darwin.go
@@ -10,8 +10,8 @@ import (
 
 func setSoftUlimit() error {
 	var rLimit syscall.Rlimit
-	rLimit.Max = 10000
-	rLimit.Cur = 10000
+	rLimit.Max = 35000
+	rLimit.Cur = 35000
 	err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
 	if err != nil {
 		return errors.Trace(err)
