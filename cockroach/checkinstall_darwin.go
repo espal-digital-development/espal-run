eef6004030cd1d1c2c0db846d44e586885693e44
Erwin <espaldd@icloud.com>
2020-07-17 11:30:45 +0200

    Small code-style update

--- cockroach/checkinstall_darwin.go
+++ cockroach/checkinstall_darwin.go
@@ -13,13 +13,14 @@ import (
 func (c *Cockroach) checkInstall() error {
 	out, _ := exec.Command("which", "cockroach").CombinedOutput()
 	isInstalled := bytes.Contains(out, []byte("/cockroach"))
-	if !isInstalled {
-		log.Println(cockroachNotFoundInstalling)
-		out, err := exec.Command("brew", "install", "cockroach").CombinedOutput()
-		if err != nil {
-			log.Println(string(out))
-			return errors.Trace(err)
-		}
+	if isInstalled {
+		return nil
+	}
+	log.Println(cockroachNotFoundInstalling)
+	out, err := exec.Command("brew", "install", "cockroach").CombinedOutput()
+	if err != nil {
+		log.Println(string(out))
+		return errors.Trace(err)
 	}
 	return nil
 }
--- cockroach/checkinstall_linux.go
+++ cockroach/checkinstall_linux.go
@@ -14,26 +14,26 @@ import (
 func (c *Cockroach) checkInstall() error {
 	out, _ := exec.Command("which", "cockroach").CombinedOutput()
 	isInstalled := bytes.Contains(out, []byte("/cockroach"))
-	if !isInstalled {
-		log.Println(cockroachNotFoundInstalling)
-		tmpDir := os.TempDir()
-		tarFileName := tmpDir + "cockroach.tgz"
-		if err := c.downloadFile(tarFileName,
-			"https://binaries.cockroachdb.com/cockroach-v20.1.3.linux-amd64.tgz"); err != nil {
-			return errors.Trace(err)
-		}
-		// cockroach-v20.1.3.linux-amd64/cockroach > cockroach
-		out, err := exec.Command("tar", "zxvf", tarFileName).CombinedOutput()
-		if err != nil {
-			log.Println(string(out))
-			return errors.Trace(err)
-		}
-		out, err = exec.Command("cp", "-i", "cockroach-v20.1.3.linux-amd64/cockroach", "/usr/local/bin/")
-			.CombinedOutput()
-		if err != nil {
-			log.Println(string(out))
-			return errors.Trace(err)
-		}
+	if isInstalled {
+		return nil
 	}
-	return nil
+	log.Println(cockroachNotFoundInstalling)
+	tmpDir := os.TempDir()
+	tarFileName := tmpDir + "cockroach.tgz"
+	if err := c.downloadFile(tarFileName,
+		"https://binaries.cockroachdb.com/cockroach-v20.1.3.linux-amd64.tgz"); err != nil {
+		return errors.Trace(err)
+	}
+	// cockroach-v20.1.3.linux-amd64/cockroach > cockroach
+	out, err := exec.Command("tar", "zxvf", tarFileName).CombinedOutput()
+	if err != nil {
+		log.Println(string(out))
+		return errors.Trace(err)
+	}
+	out, err = exec.Command("cp", "-i", "cockroach-v20.1.3.linux-amd64/cockroach", "/usr/local/bin/").
+		CombinedOutput()
+	if err != nil {
+		log.Println(string(out))
+		return errors.Trace(err)
+	}
 }
